a449f8f09bef04567519e17c7efe346798a4f748
ITP312 <itp312@l632-62.local>
2017-04-25 09:25:51 +0800

    Initial Commit

---
+++ TabBarApp/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  TabBarApp
+//
+//  Created by ITP312 on 25/4/17.
+//  Copyright © 2017 ITP312. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
